5677d5aceb2d194777d09aebd7070d5000f117c9
Thanatus <s.sidoryaka@gmail.com>
2023-06-18 17:19:09 +0300

    feat: add some tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -137,8 +137,17 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function fn(...args) {
+    const functionName = func.name || 'anonymous';
+    const argString = args.map((arg) => JSON.stringify(arg)).join(',');
+
+    logFunc(`${functionName}(${argString}) starts`);
+    const result = func.apply(this, args);
+    logFunc(`${functionName}(${argString}) ends`);
+
+    return result;
+  };
 }
 
 /**
